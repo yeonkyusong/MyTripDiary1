3fc3deb52674f20c5d7d9a83118db8a05e24f720
mincheol. bae <kimmincheol99@gmail.com>
2023-11-17 17:31:49 +0900

    2023-11-17 17:31

--- app/src/main/java/com/teamproject/mytripdiary/SetLocation.java
+++ app/src/main/java/com/teamproject/mytripdiary/SetLocation.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SetLocation extends AppCompatActivity {
+    private ImageButton btn_home;
 
 
 
@@ -16,7 +18,16 @@ public class SetLocation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_location);
 
-        
+
+        btn_home = findViewById(R.id.btn_home);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go_home= new Intent(SetLocation.this , MainActivity.class);
+                startActivity(go_home);
+            }
+        });
+
 
     }
 }
