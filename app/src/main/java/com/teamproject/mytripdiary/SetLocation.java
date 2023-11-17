6ff2775e330e30578efffc22ef8bb9a37627dcab
tripgyu <songsj2040@Naver.com>
2023-11-17 18:49:50 +0900

    tripn

--- app/src/main/java/com/teamproject/mytripdiary/SetLocation.java
+++ app/src/main/java/com/teamproject/mytripdiary/SetLocation.java
@@ -1,6 +1,8 @@
 package com.teamproject.mytripdiary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.CameraUpdateFactory;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,9 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class SetLocation extends AppCompatActivity {
-    private ImageButton btn_home;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class SetLocation extends AppCompatActivity implements OnMapReadyCallback{
+    private ImageButton btn_home;
+    private MapView mapView;
+    private GoogleMap googleMap;
 
 
     @Override
@@ -18,6 +27,9 @@ public class SetLocation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_location);
 
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
         btn_home = findViewById(R.id.btn_home);
         btn_home.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +39,36 @@ public class SetLocation extends AppCompatActivity {
                 startActivity(go_home);
             }
         });
-
-
     }
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+
+        // 지도 초기 설정 등을 수행
+        // 예: googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(37.7749, -122.4194)));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
 }
